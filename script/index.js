0b873a89fc2a27934f9923a0ec21ac89f073653e
Andrey Rusanov <rusanow.andrej2018@yandex.ru>
2023-08-11 20:41:05 +0300

    feat: open and close popup for add cards

--- script/index.js
+++ script/index.js
@@ -10,18 +10,18 @@ const jobInput = editFormElement.querySelector('.popup__input_field_job');
 const nameProfile = document.querySelector('.profile__name');
 const jobProfile = document.querySelector('.profile__description');
 
-//Открытие popup
+//Открытие popup на редактирование профиля
 const editProfileButton = document.querySelector('.profile__button_type_edit');
-function openPopup() {
-  addPopupElement.classList.add('popup_opened');
+function openPopupEdit() {
+  editPopupElement.classList.add('popup_opened');
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
 
-//Закрытие popup
-let closePopupButton = document.querySelector('.popup__close-button');
-function closePopup() {
-  addPopupElement.classList.remove('popup_opened');
+//Закрытие popup на редактирование профиля
+let closePopupButtonEdit = document.querySelector('#close-edit');
+function closePopupEdit() {
+  editPopupElement.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
@@ -30,7 +30,7 @@ function handleFormSubmit(evt) {
   // О том, как это делать, расскажем позже.
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  closePopup();
+  closePopupEdit();
 }
 //=====Sprint5========================
 // Находим попап для добавления новой карточки в DOM
@@ -43,8 +43,34 @@ const addFormElement = document.querySelector('#add-form');
 const namePlace = addFormElement.querySelector('.popup__input_field_palce');
 const nameLink = addFormElement.querySelector('.popup__input_field_link');
 
+//Открытие popup на добавление карточек
+const addProfileButton = document.querySelector('.profile__button_type_add');
+function openPopupAdd() {
+  addPopupElement.classList.add('popup_opened');
+  // nameInput.value = nameProfile.textContent;
+  // jobInput.value = jobProfile.textContent;
+}
+
+//Закрытие popup на редактирование профиля
+let closePopupButtonAdd = document.querySelector('#close-add');
+function closePopupAdd() {
+  addPopupElement.classList.remove('popup_opened');
+}
+
+// function handleFormSubmit(evt) {
+//   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+//   // Так мы можем определить свою логику отправки.
+//   // О том, как это делать, расскажем позже.
+//   nameProfile.textContent = nameInput.value;
+//   jobProfile.textContent = jobInput.value;
+//   closePopup();
+// }
+
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 editFormElement.addEventListener('submit', handleFormSubmit);
-editProfileButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', closePopup);
+editProfileButton.addEventListener('click', openPopupEdit);
+closePopupButtonEdit.addEventListener('click', closePopupEdit);
+
+addProfileButton.addEventListener('click', openPopupAdd);
+closePopupButtonAdd.addEventListener('click', closePopupAdd);
