bac1bacfcdcaedd053b3d5bbb835d9f7440ca17b
Andrey Rusanov <rusanow.andrej2018@yandex.ru>
2023-10-04 09:21:32 +0300

    feat: add Section class

---
+++ script/Section.js
@@ -0,0 +1,19 @@
+class Section {
+  constructor({ items, renderer }, sectionSelector) {
+    this._initialArray = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(sectionSelector);
+  }
+
+  //   публичный метод, который принимает DOM-элемент и добавляет его в контейнер
+  addItem(item) {
+    const newElement = this._renderer(item);
+    this._container.prepend(newElement);
+  }
+
+  renderItem() {
+    this._initialArray.forEach(item => {
+      this.addItem(item);
+    });
+  }
+}
