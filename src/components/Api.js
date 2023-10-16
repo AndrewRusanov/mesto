b19e552364d0ee12946b1108cf642dfa453a8dd9
Andrey Rusanov <rusanow.andrej2018@yandex.ru>
2023-10-16 16:19:37 +0300

    feat: add initial cards request

--- src/components/Api.js
+++ src/components/Api.js
@@ -4,6 +4,7 @@ export default class Api {
     this._token = headers['authorization'];
     this.headers = headers;
   }
+
   // Загрузка информации о пользователе с сервера
   getUserInformation() {
     return fetch(`${this.baseUrl}/users/me`, {
@@ -18,8 +19,22 @@ export default class Api {
           return Promise.reject(`Ошибка: ${res.status}`);
         }
       })
-      .then(result => {
-        return result;
+      .catch(err => console.log(err));
+  }
+
+  // Загрузак карточек с сервера
+  getCards() {
+    return fetch(`${this.baseUrl}/cards`, {
+      headers: {
+        authorization: this._token
+      }
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return Promise.reject(`Ошибка: ${res.status}`);
+        }
       })
       .catch(err => console.log(err));
   }
