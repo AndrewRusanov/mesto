fc338c8cc7acd4cf8fc64117384929312c425c55
Andrey Rusanov <rusanow.andrej2018@yandex.ru>
2023-10-17 18:49:23 +0300

    feat: only owner trashButton

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,21 +3,24 @@ class UserInfo {
     this._nameProfile = document.querySelector(nameProfileSelector);
     this._descriptionProfile = document.querySelector(descriptionProfileSelector);
     this._avatarProfile = document.querySelector(avatarProfileSelector);
+    this._userId = '';
   }
 
   //   публичный метод, который возвращает объект с данными пользователя (для подтягивания данных из профиля в попап)
   getUserInfo() {
     return {
       name: this._nameProfile.textContent,
-      description: this._descriptionProfile.textContent
+      description: this._descriptionProfile.textContent,
+      userId: this._userId
     };
   }
 
   //   публичный метод, который принимает новые данные пользователя и добавляет их на страницу (для подтягивания данных из попапа в профиль)
-  setUserInfo({ name, about, avatar }) {
+  setUserInfo({ name, about, avatar, _id }) {
     this._nameProfile.textContent = name;
     this._descriptionProfile.textContent = about;
     this._avatarProfile.src = avatar;
+    this._userId = _id;
   }
 }
 
